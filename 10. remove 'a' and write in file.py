d3c6aeb93f9b793d20c7044ea7104b2b1c8d8595
Vinu <vinu@Vinus-MacBook-Air.local>
2021-11-24 22:51:38 +0530

    First

--- 10. remove 'a' and write in file.py
+++ 10. remove 'a' and write in file.py
@@ -1,21 +1,15 @@
-#To be completed
-
 f = open("test.txt",'r') 
 g = open("text2.txt",'w') 
 str = " "
 listoflines = f.readlines()
-for line in listoflines:
-    words = line.split()
-    for i in words:
-        letter = i.split()[0]
+for list in listoflines:
+    curlist = list.split()
+    if "a" in curlist:
+        print("a is present")
+    else:
+        g.write(list)
 
-
-
-# while str:
-#     str = f.readline() 
-#     if 'a' in str:
-#         g.write(str)
-# f.close() 
-# g.close()
+f.close() 
+g.close()
 
 
